20aa14a70ea12505e7d9159a7e093e8c6cd09ca2
Huy Nguyen <ng.q.huy98@gmail.com>
2024-10-09 21:32:53 +0300

    basic styling

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,10 +9,20 @@ const CreatePost = () => {
     }
 
     return (
-        <div>
-            <h1> Create Post </h1>
-            <p> Title </p>s
-            <input id="title_input" value={title} onChange={onChange}/>
+        <div className="text-left ml-10 mt-10">
+            <h1 className="font-bold text-xl"> Create Post </h1>
+            <form className="w-64">
+                <div className="block">
+                    <p className="text-base inline"> Title </p>
+                    <input 
+                        className="inline ml-[10px] border-2 border-black" 
+                        id="title_input" 
+                        value={title} 
+                        onChange={onChange}
+                    />
+                </div>
+                <button className="block float-right"> Post </button>
+            </form>
         </div>
     );
 }
